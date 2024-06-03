bf797cdfaf061d2a9da8de2665db7f258e3bf871
BibekNeupane51 <bibekneupane51@gmail.com>
2024-06-03 23:30:57 +0545

    frontend copied from repo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,39 @@
-import react from "react";
-import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
-import Login from "./pages/Login.jsx";
-import Register from "./pages/Register.jsx";
-import Home from "./pages/Home.jsx";
-import NotFound from "./pages/NotFound.jsx";
-import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import react from "react"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+import ProtectedRoute from "./components/ProtectedRoute"
 
 function Logout() {
   localStorage.clear()
-  return <Navigate to="/login"/>
+  return <Navigate to="/login" />
 }
 
 function RegisterAndLogout() {
   localStorage.clear()
-  return <Register/>
+  return <Register />
 }
-function App() {
 
+function App() {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<ProtectedRoute><Home/></ProtectedRoute>} />
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/Register" element={<Register/>}/>
-            <Route path="*" element={<NotFound/>}/>
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,6 +1,8 @@
 function NotFound() {
     return <div>
-        NotFound
+        <h1>404 Not Found</h1>
+        <p>The page you're looking for doesn't exist!</p>
     </div>
 }
+
 export default NotFound
